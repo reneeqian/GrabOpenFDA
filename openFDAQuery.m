f161356f620b7f301182284ba7aa4f0a48b5379d
rqian <rqian@mathworks.com>
2022-06-08 22:45:20 -0700

    * added query for recalls

--- openFDAQuery.m
+++ openFDAQuery.m
@@ -5,23 +5,49 @@ data = webread(searchQuery);
 % recalls = table;
 
 for ii = 1:numel(data.results)
-    
-    recalls(ii).event_date_posted = data.results{ii}.event_date_posted;
+    recalls(ii).event_date_posted = datetime(data.results{ii}.event_date_posted);
     recalls(ii).recall_status = data.results{ii}.recall_status;
-    recalls(ii).product_code = data.results{ii}.product_code;
+
+    if isfield(data.results{ii},'product_code')
+        recalls(ii).product_code = data.results{ii}.product_code;
+    else
+        recalls(ii).product_code = '';
+    end
+
     recalls(ii).product_description = data.results{ii}.product_description;
-    recalls(ii).code_info = data.results{ii}.code_info;
-    recalls(ii).recalling_firm = data.results{ii}.recalling_firm;
-    recalls(ii).address_1 = data.results{ii}.address_1;
-    recalls(ii).city = data.results{ii}.city;
-    recalls(ii).state = data.results{ii}.state;
-    recalls(ii).postal_code = data.results{ii}.postal_code;
-    recalls(ii).additional_info_contact = data.results{ii}.additional_info_contact;
+
+    if isfield(data.results{ii},'code_info')
+        recalls(ii).code_info = data.results{ii}.code_info;
+    else
+        recalls(ii).code_info = '';
+    end
+    if isfield(data.results{ii},'recalling_firm')
+        recalls(ii).recalling_firm = data.results{ii}.recalling_firm;
+    else
+        recalls(ii).recalling_firm = '';
+    end
+
+    %     recalls(ii).additional_info_contact = data.results{ii}.additional_info_contact;
     recalls(ii).reason_for_recall = data.results{ii}.reason_for_recall;
     recalls(ii).root_cause_description = data.results{ii}.root_cause_description;
-    recalls(ii).action = data.results{ii}.action;
-    recalls(ii).product_quantity = data.results{ii}.product_quantity;
-    recalls(ii).distribution_pattern = data.results{ii}.distribution_pattern;
-    recalls(ii).other_submission_description = data.results{ii}.other_submission_description;
-    recalls(ii).openfda = data.results{ii}.openfda;
+
+    if isfield(data.results{ii},'action')
+        recalls(ii).action = data.results{ii}.action;
+    else
+        recalls(ii).action = '';
+    end
+    %     recalls(ii).product_quantity = data.results{ii}.product_quantity;
+    %     recalls(ii).distribution_pattern = data.results{ii}.distribution_pattern;
+    %     recalls(ii).other_submission_description = data.results{ii}.other_submission_description;
+    if isfield(data.results{ii}.openfda,'device_class') && ~isstrprop(data.results{ii}.openfda.device_class,'alpha')
+        recalls(ii).DeviceClass = str2num(data.results{ii}.openfda.device_class);
+        recalls(ii).MedicalSpecialtyDescription = data.results{ii}.openfda.medical_specialty_description;
+    else
+        recalls(ii).DeviceClass = nan;
+        recalls(ii).MedicalSpecialtyDescription = '';
+    end
 end
+
+recallTable = struct2table(recalls);
+%%
+deviceRecalls = recallTable(~isnan(recallTable.DeviceClass),:);
